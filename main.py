05d08d00fec7acf043f06460867c19987f31fdd8
Tiego75 <thibaut.gautier@edu.esiee.fr>
2025-11-13 10:05:01 +0000

    Exo Palindromes

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
-#### Fonction secondaire
+"""
+this module check if the words in the list are palindromes
+"""
 
 
 def ispalindrome(p):
+    """
+    Return true if the word is a palindrome
 
-    # votre code ici
-    
-    return False
+    Args:
+        p(str): the string to test
+    Return: 
+        bool: True if the word is a palindrome, False if not
+    """
+
+
+    table = str.maketrans("éèêëàâùûçôîïâ","eeeeaauucoiia")
+    p = p.lower().translate(table)
+    for punctuation in ' \'!,.?;:"-':
+        p = p.replace(punctuation, "")
+
+    for j in range(len(p)//2):
+        if p[j]!=p[-j-1]:
+            return False
+    return True
+
+
 
 #### Fonction principale
-
-
 def main():
-
+    """
+    Check if the words in the list are palindromes
+    """
     # vos appels à la fonction secondaire ici
-
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
-
-
 if __name__ == "__main__":
     main()
